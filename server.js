f4e464ce8d71253c913bd1715c14bf8c5e96118a
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-12 14:02:58 +0100

    Create Posts Array

--- server.js
+++ server.js
@@ -9,3 +9,36 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send("Server del mio blog");
 })
+
+const posts = [
+    {
+        titol: "Ciambellone" ,
+        content: "Ricetta Ciambellone" ,
+        image: "/images/ciambellone.jpeg",
+        tag: ["dolce", "ricetta", "fattoincasa"]
+    },
+    {
+        titol: "Cracker alla Barbabietola" ,
+        content: "Ricetta Cracker" ,
+        image: "/images/cracker_barbabietola.jpeg",
+        tag: ["dolce", "ricetta", "fattoincasa"]
+    },
+    {
+        titol: "Pane Fritto Dolce" ,
+        content: "Ricetta Pane" ,
+        image: "/images/pane_fritto_dolce.jpeg",
+        tag: ["dolce", "ricetta", "fattoincasa"]
+    },
+    {
+        titol: "Pasta Barbabietola" ,
+        content: "Ricetta Pasta" ,
+        image: "/images/pasta_barbabietola.jpeg",
+        tag: ["dolce", "ricetta", "fattoincasa"]
+    },
+    {
+        titol: "Torta Paesana" ,
+        content: "Ricetta Torta" ,
+        image: "/images/torta_paesana.jpeg",
+        tag: ["dolce", "ricetta", "fattoincasa"]
+    }
+]
